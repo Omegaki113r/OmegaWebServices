0a80ccda64096e502db566868e0cf6d15473a18b
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2025-03-16 00:47:43 +0530

    just a minor refactor to remove the ChatGPT comments

--- extra/scripts/node/mqtt-server/mqtt-server.js
+++ extra/scripts/node/mqtt-server/mqtt-server.js
@@ -3,7 +3,8 @@
  * @author Omegaki113r
  * @date Monday, 10th February 2025 3:05:16 am
  * @copyright Copyright 2025 - 2025 0m3g4ki113r, Xtronic
- * *//*
+ * */
+/*
 * Project: mqtt-server
 * File Name: mqtt-server.js
 * File Created: Monday, 10th February 2025 3:05:16 am
@@ -19,18 +20,14 @@
 * ----------	---	---------------------------------------------------------
 */
 
-const aedes = require('aedes')(); // Import and create an instance of the aedes MQTT broker
-const net = require('net'); // To create a TCP server
+const aedes = require('aedes')();
+const net = require('net');
 
-// Create a TCP server that listens on port 1883
 const server = net.createServer(aedes.handle);
-
-// Make the server listen on port 1883 (MQTT default port)
 server.listen(1883, () => {
     console.log('MQTT broker is running on port 1883');
 });
 
-// Setup the broker's on 'publish' event to log when a message is published
 aedes.on('publish', (packet, client) => {
     if (client) {
         const topic = packet.topic;
@@ -54,12 +51,10 @@ aedes.on('subscribe', (subscription, client) => {
     console.log(subscription[0].topic + ": " + subscription[0].qos);
 });
 
-// Handle incoming connections from clients
 aedes.on('client', (client) => {
     console.log('Client Connected:', client.id);
 });
 
-// Handle client disconnections
 aedes.on('clientDisconnect', (client) => {
     console.log('Client Disconnected:', client.id);
 });
